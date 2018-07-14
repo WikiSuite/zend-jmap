e2016b5fc2f3095e3ee7c9e0feb2f4132ff12722
Benoit Grégoire <benoitg@coeus.ca>
2018-07-14 19:25:25 -0400

    Working back references

--- libjmap/src/jmap-mail.php
+++ libjmap/src/jmap-mail.php
@@ -5,19 +5,23 @@ namespace Wikisuite\JMAPMail;
 require 'request.php';
 use Wikisuite\JMAPCore;
 use Wikisuite\JMAPCore\JMAPRequest;
+use Wikisuite\JMAPCore\ResultReference;
 
-class Mailbox {
-  private $connection;
+class Mailbox
+{
+    private $connection;
 
-  public function __construct($connection){
-    $this->connection = $connection;
-  }
-  public function getInbox() {
-    $filter =  array('hasRole' => 'inbox');
-    $request = new JMAPRequest($this->connection);
-    $request->addQuery("Mailbox", $filter);
-    $request->send();
-    
-
-  }
+    public function __construct($connection)
+    {
+        $this->connection = $connection;
+    }
+    public function getInbox()
+    {
+        $filter =  array('filter'=>array('hasRole' => true));
+        $request = new JMAPRequest($this->connection);
+        $mailboxeWithroles = $request->addQuery('Mailbox', $filter);
+        $previousIds = new ResultReference("/ids", $mailboxeWithroles);
+        $request->addMethodCall('Mailbox/get', array('#ids'=>$previousIds));
+        $request->send();
+    }
 }
--- libjmap/src/request.php
+++ libjmap/src/request.php
@@ -3,6 +3,25 @@ namespace Wikisuite\JMAPCore;
 
 use Zend\Http\Request;
 
+class ResultReference
+{
+    private $name;
+    private $path;
+    private $resultOf;
+    public function __construct($path, $backReferencedMethodCall)
+    {
+        $this->path = $path;
+        $this->backReferencedMethodCall = $backReferencedMethodCall;
+    }
+    public function toRequest()
+    {
+        return (object) [
+          'name' => $this->backReferencedMethodCall['name'],
+          'path' => $this->path,
+          'resultOf'=>$this->backReferencedMethodCall['client_id']];
+    }
+}
+
 class JMAPRequest
 {
     protected $methodCalls = [];
@@ -11,29 +30,45 @@ class JMAPRequest
     {
         $this->connection = $connection;
     }
-    private function addMethodCall($name, $arguments)
+    public function addMethodCall($name, $arguments)
     {
         $methodCall = [];
         $methodCall['name'] = $name;
         $methodCall['arguments'] = $arguments;
         $methodCall['client_id'] = uniqid();
         array_push($this->methodCalls, $methodCall);
+        return $methodCall;
     }
+
 
     public function addQuery($object, $filter)
     {
         /*if (!in_array($filterOperator, ['AND','OR','NOT'])) {
             throw new \Exception("Invalid filter operator");
         }*/
-        $this->addMethodCall($object.'/query', $filter);
+        return $this->addMethodCall($object.'/query', $filter);
     }
+
+    private function methodCallArgumentToRequest($argument)
+    {
+        if ($argument instanceof ResultReference) {
+            $retVal = $argument->toRequest();
+        } else {
+            $retVal = $argument;
+        }
+
+        return $retVal;
+    }
+
 
     private function methodCallToRequest($methodCall)
     {
         return array($methodCall['name'],
-       $methodCall['arguments'],
+       array_map(array($this, 'methodCallArgumentToRequest'), $methodCall['arguments']),
        $methodCall['client_id']);
     }
+
+
     public function toJson()
     {
         $rawRequest =   array(
